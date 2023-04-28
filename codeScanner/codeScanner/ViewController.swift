2e6356ec88ff28d6f80fde4979389809dd52be7c
Oriol Sànchez Miró <oriol.sanchez@sita.aero>
2023-04-28 12:04:00 +0200

    commit for new mac

---
+++ codeScanner/codeScanner/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  codeScanner
+//
+//  Created by Oriol Sanchez Miro on 31/8/22.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Scan(_ sender: Any) {
+        
+    }
+    
+}
+
